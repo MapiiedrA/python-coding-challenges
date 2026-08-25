8cc67d118d3f938e8b9c64686d82e857dfb730b9
Mauricio Gutierrez Piedra <mapiedra3223@gmail.com>
2026-08-25 14:02:33 -0600

    Add files via upload

---
+++ 00-python-basics.py
@@ -0,0 +1,33 @@
+# https://python.org/
+# this is a commentary on a line
+"""
+This is a multi-line comment
+or documentation string (docstring)
+used to describe code blocks.
+"""
+
+'''
+This is a multi-line comment
+or documentation string (docstring)
+used to describe code blocks.
+'''
+
+my_variable = "My Variable"
+my_variable = "New value of My Variable"
+#python dont have Constant
+
+My_Constant = "Mi constant" # By convention, constants are written in uppercase 
+
+my_int = 1 #number without Double quotes for a int
+my_float = 1.5 #Decimals
+my_bool = True #logic boolean
+my_bool = False
+my_string = "My String Text"
+my_other_string = 'My other String Text'
+
+print("Hello, Python!")
+      
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
